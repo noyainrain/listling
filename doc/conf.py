a544afc97d882678726194cae2bdd250e748b830
Sven <sven@inrain.org>
2019-02-07 18:58:51 +0100

    Fix broken push notifications on Firefox mobile
    Therefor update micro.
    
    Close #38.

--- doc/conf.py
+++ doc/conf.py
@@ -11,7 +11,7 @@ master_doc = 'index'
 
 project = 'Open Listling'
 copyright = '2018 Open Listling contributors'
-version = release = '0.8.1'
+version = release = '0.8.2'
 
 html_theme_options = {
     'logo': 'listling.svg',
